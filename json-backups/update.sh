c4cb58a472a102ead7c14c62e9481990ede6259a
STAM <developer@epicm.org>
2021-01-15 01:41:33 +0300

    updated scripts

--- json-backups/update.sh
+++ json-backups/update.sh
@@ -7,28 +7,28 @@ mkdir -p $PWD/current $PWD/archived $PWD/eap
 ###################################################################################################
 
 # Bamboo
-wget -c --random-wait -P $PWD/current/ https://my.atlassian.com/download/feeds/current/bamboo.json
+wget -c -nv --random-wait -P $PWD/current/ https://my.atlassian.com/download/feeds/current/bamboo.json
 
 # Bitbucket (Stash)
-wget -c --random-wait -P $PWD/current/ https://my.atlassian.com/download/feeds/current/stash.json
+wget -c -nv --random-wait -P $PWD/current/ https://my.atlassian.com/download/feeds/current/stash.json
 
 # Confluence
-wget -c --random-wait -P $PWD/current/ https://my.atlassian.com/download/feeds/current/confluence.json
+wget -c -nv --random-wait -P $PWD/current/ https://my.atlassian.com/download/feeds/current/confluence.json
 
 # Crowd
-wget -c --random-wait -P $PWD/current/ https://my.atlassian.com/download/feeds/current/crowd.json
+wget -c -nv --random-wait -P $PWD/current/ https://my.atlassian.com/download/feeds/current/crowd.json
 
 # Crucible
-wget -c --random-wait -P $PWD/current/ https://my.atlassian.com/download/feeds/current/crucible.json
+wget -c -nv --random-wait -P $PWD/current/ https://my.atlassian.com/download/feeds/current/crucible.json
 
 # FishEye
-wget -c --random-wait -P $PWD/current/ https://my.atlassian.com/download/feeds/current/fisheye.json
+wget -c -nv --random-wait -P $PWD/current/ https://my.atlassian.com/download/feeds/current/fisheye.json
 
 # Jira
-#wget -c --random-wait -P $PWD/current/ https://my.atlassian.com/download/feeds/current/jira.json
-wget -c --random-wait -P $PWD/current/ https://my.atlassian.com/download/feeds/current/jira-core.json
-wget -c --random-wait -P $PWD/current/ https://my.atlassian.com/download/feeds/current/jira-software.json
-wget -c --random-wait -P $PWD/current/ https://my.atlassian.com/download/feeds/current/jira-servicedesk.json
+#wget -c -nv --random-wait -P $PWD/current/ https://my.atlassian.com/download/feeds/current/jira.json
+wget -c -nv --random-wait -P $PWD/current/ https://my.atlassian.com/download/feeds/current/jira-core.json
+wget -c -nv --random-wait -P $PWD/current/ https://my.atlassian.com/download/feeds/current/jira-software.json
+wget -c -nv --random-wait -P $PWD/current/ https://my.atlassian.com/download/feeds/current/jira-servicedesk.json
 
 
 ###################################################################################################
@@ -36,36 +36,36 @@ wget -c --random-wait -P $PWD/current/ https://my.atlassian.com/download/feeds/c
 ###################################################################################################
 
 # Bamboo
-wget -c --random-wait -P $PWD/archived/ https://my.atlassian.com/download/feeds/archived/bamboo.json
+wget -c -nv --random-wait -P $PWD/archived/ https://my.atlassian.com/download/feeds/archived/bamboo.json
 
 # Bitbucket (Stash)
-wget -c --random-wait -P $PWD/archived/ https://my.atlassian.com/download/feeds/archived/stash.json
+wget -c -nv --random-wait -P $PWD/archived/ https://my.atlassian.com/download/feeds/archived/stash.json
 
 # Confluence
-wget -c --random-wait -P $PWD/archived/ https://my.atlassian.com/download/feeds/archived/confluence.json
+wget -c -nv --random-wait -P $PWD/archived/ https://my.atlassian.com/download/feeds/archived/confluence.json
 
 # Crowd
-wget -c --random-wait -P $PWD/archived/ https://my.atlassian.com/download/feeds/archived/crowd.json
+wget -c -nv --random-wait -P $PWD/archived/ https://my.atlassian.com/download/feeds/archived/crowd.json
 
 # Crucible
-wget -c --random-wait -P $PWD/archived/ https://my.atlassian.com/download/feeds/archived/crucible.json
+wget -c -nv --random-wait -P $PWD/archived/ https://my.atlassian.com/download/feeds/archived/crucible.json
 
 # FishEye
-wget -c --random-wait -P $PWD/archived/ https://my.atlassian.com/download/feeds/archived/fisheye.json
+wget -c -nv --random-wait -P $PWD/archived/ https://my.atlassian.com/download/feeds/archived/fisheye.json
 
 # Jira
-wget -c --random-wait -P $PWD/archived/ https://my.atlassian.com/download/feeds/archived/jira.json
-wget -c --random-wait -P $PWD/archived/ https://my.atlassian.com/download/feeds/archived/jira-core.json
-wget -c --random-wait -P $PWD/archived/ https://my.atlassian.com/download/feeds/archived/jira-software.json
-wget -c --random-wait -P $PWD/archived/ https://my.atlassian.com/download/feeds/archived/jira-servicedesk.json
+wget -c -nv --random-wait -P $PWD/archived/ https://my.atlassian.com/download/feeds/archived/jira.json
+wget -c -nv --random-wait -P $PWD/archived/ https://my.atlassian.com/download/feeds/archived/jira-core.json
+wget -c -nv --random-wait -P $PWD/archived/ https://my.atlassian.com/download/feeds/archived/jira-software.json
+wget -c -nv --random-wait -P $PWD/archived/ https://my.atlassian.com/download/feeds/archived/jira-servicedesk.json
 
 
 ###################################################################################################
 # EAP links
 ###################################################################################################
-wget -c --random-wait -P $PWD/eap/ https://my.atlassian.com/download/feeds/eap/bamboo.json
-wget -c --random-wait -P $PWD/eap/ https://my.atlassian.com/download/feeds/eap/confluence.json
-wget -c --random-wait -P $PWD/eap/ https://my.atlassian.com/download/feeds/eap/jira.json
-wget -c --random-wait -P $PWD/eap/ https://my.atlassian.com/download/feeds/eap/jira-servicedesk.json
-wget -c --random-wait -P $PWD/eap/ https://my.atlassian.com/download/feeds/eap/stash.json
+wget -c -nv --random-wait -P $PWD/eap/ https://my.atlassian.com/download/feeds/eap/bamboo.json
+wget -c -nv --random-wait -P $PWD/eap/ https://my.atlassian.com/download/feeds/eap/confluence.json
+wget -c -nv --random-wait -P $PWD/eap/ https://my.atlassian.com/download/feeds/eap/jira.json
+wget -c -nv --random-wait -P $PWD/eap/ https://my.atlassian.com/download/feeds/eap/jira-servicedesk.json
+wget -c -nv --random-wait -P $PWD/eap/ https://my.atlassian.com/download/feeds/eap/stash.json
 
